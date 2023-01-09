51bd5f1f63493dd16687c65c748c3a295b0aada9
Denisyasyuchenya <yasyuchenya1@gmail.com>
2023-01-09 03:52:13 +0300

    feat: add to readable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
+import { decs } from "./constant";
+
 module.exports = function toReadable (number) {
-  
+const once = number.toString();
+const hundred = number % 100;
+const decimal = number % 10;
+let result = '';
+
+if(once.length === 3){
+  if (decimal === 0) {
+    result+= decs[(number - hundred)  / 100] + ' hundred ';
+    if(hundred !== 0){
+      result+= decs[hundred - decimal]
+    }
+  }
+  if (decimal !== 0) {
+    result += decs[(number - hundred)  / 100] + ' hundred';
+    if (hundred <= 19) {
+      result += ' ' + decs[hundred];
+    }
+    if (hundred > 19) {
+      result += ' ' + decs[hundred - decimal] + ' ' + decs[decimal];
+  }
+}    
+  return result.trim();
+}
+
+if(once.length === 2){
+  if(number <= 19){
+    return decs[number]
+  }else{
+    if(decimal === 0){
+      result += decs[number]
+    }
+   if(decimal !== 0){
+    result+= decs[number - decimal] + ' ' + decs[decimal]
+     
+   }
+   return result.trim()
+  }
+}
+return decs[number]
 }
